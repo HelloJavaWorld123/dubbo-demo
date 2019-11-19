47609a591fbfec51dc304a900b89f1f89a8528df
xiaokuan.ren <renxiaokuan9428@duia.com>
2019-11-19 11:31:27 +0800

    项目基本结构

---
+++ dubbo-consumer/src/main/java/com/dubbo/demo/dubboconsumer/DubboConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.dubbo.demo.dubboconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author admin
+ */
+@SpringBootApplication
+public class DubboConsumerApplication{
+
+	public static void main(String[] args){
+		SpringApplication.run(DubboConsumerApplication.class, args);
+	}
+
+}
---
+++ dubbo-provider/src/main/java/com/dubbo/demo/dubboprovider/DubboProviderApplication.java
@@ -0,0 +1,16 @@
+package com.dubbo.demo.dubboprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author admin
+ */
+@SpringBootApplication
+public class DubboProviderApplication{
+
+	public static void main(String[] args){
+		SpringApplication.run(DubboProviderApplication.class, args);
+	}
+
+}
